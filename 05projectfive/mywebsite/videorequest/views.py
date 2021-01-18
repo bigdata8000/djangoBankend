956f7ebcca0d41c8c1f0ffa402a1b73701ad714c
henrymmmm <henrymmmm@gmail.com>
2021-01-18 14:53:12 +0000

    finished prj 5

--- 05projectfive/mywebsite/videorequest/views.py
+++ 05projectfive/mywebsite/videorequest/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Video
+from .forms import VideoForm
 
 def index(request):
-    return render(request, 'videorequest/index.html')
+    videos = Video.objects.order_by('-date_added')
+    context = {'videos': videos}
+    return render(request, 'videorequest/index.html', context)
 
 
 def vrform(request):
-    return render(request, 'videorequest/vrform.html')    
+    if request.method == 'POST':
+        form = VideoForm(request.POST)
+    
+        if form.is_valid():
+            new_req = Video(videotitle=request.POST['videoname'], videodesc=request.POST['videodesc'])
+            new_req.save()
+            return redirect('index')
+    else:
+        form = VideoForm()
 
+    context = {'form': form}
+
+    return render(request, 'videorequest/vrform.html', context)    
 
